41115a35b1fe044a998254ab3fd926ee9e57f091
Hieuducemo <115462421+Hieuducemo@users.noreply.github.com>
2023-01-20 08:12:30 +0100

    Add project files.

---
+++ firstsdl/defs.h
@@ -0,0 +1,28 @@
+
+#ifndef DEFS_H_
+#define DEFS_H_
+
+#define WIDTH 500
+#define HEIGHT 700
+
+#define FPS 60
+#define frameDelay 100 / FPS
+
+#define GRAVITY 80.0f
+
+#define BIRD_WIDTH 55
+#define BIRD_HEIGHT 39
+
+#define PIPE_WIDTH 80
+#define PIPE_WIDTH_SRC 52
+#define PIPE_GAP 170
+#define PIPE_DISTANCE 250
+#define PIPE_V 3
+
+#define PIPE_MIN_H 150
+#define PIPE_MAX_H 450
+
+#define GROUND_HEIGHT 112
+#define GROUND_WIDTH 336
+
+#endif
