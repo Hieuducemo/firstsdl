981c48843e2c45bf8d60d2148ef5195960f11fce
Anya Forger <115462421+Hieuducemo@users.noreply.github.com>
2023-01-24 21:57:39 +0100

    Update Bird.h

--- firstsdl/Bird.h
+++ firstsdl/Bird.h
@@ -8,15 +8,17 @@
 class Bird
 {
 private:
+    // creat velocity and acceleration while the fish is in action jump or fall
     float velocity;
     float acceleration;
-
+    // A structure that contains the definition of a rectangle, with the origin at the upper left.
     SDL_Rect pos;
-
+    // states of the fish 
     SDL_Texture* up;
     SDL_Texture* down;
     SDL_Texture* mid;
     SDL_Texture* currentRenderingTexture;
+    // show the texture on the created windom 
     SDL_Renderer* renderer;
 
     int animationFrames;
